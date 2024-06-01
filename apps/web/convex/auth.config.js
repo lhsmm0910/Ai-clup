e2d69c0fd349d947e75676d29f424e1002dad64f
luces <lubo@cqop.com>
2024-06-01 21:50:38 +0800

    pp11

--- apps/web/convex/auth.config.js
+++ apps/web/convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: 'https://cheerful-goat-45.clerk.accounts.dev',
       applicationID: "convex",
     },
   ],
